c85f39383303eb4f334592a9695c4134b1499197
dhacker29 <dhackerdvm@gmail.com>
2014-06-06 03:01:35 -0500

    Enable -f-no-strict-aliasing for audio.primary
    Change-Id: Id95e30e0d5a6a5e8e14bb549ca18b4a7d1223f4a

--- audio/Android.mk
+++ audio/Android.mk
@@ -16,6 +16,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_CFLAGS += -fno-strict-aliasing
 LOCAL_MODULE := audio.primary.tuna
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SRC_FILES := audio_hw.c ril_interface.c
